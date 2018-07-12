cb0e7ddf609e9aeeb7061486a47d00b524868841
Konstantin Tomashevich <ekit.mail@gmail.com>
2018-07-12 19:47:51 +0300

    Add basic Urho3DHexagonalGrid example.

---
+++ HexagonalGrids/Urho3DHexagonalGrid.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <Urho3D/Engine/Application.h>
+#include <Urho3D/Scene/Scene.h>
+#include <Urho3DDebugCamera/DebugCamera.hpp>
+#include <SharedPointer/ReferenceCounted.hpp>
+#include <HexagonalGrids/DoubledCoordsHexGrid.hpp>
+
+class Urho3DHexagonalGrid : public Urho3D::Application
+{
+    URHO3D_OBJECT (Urho3DHexagonalGrid, Application)
+public:
+    Urho3DHexagonalGrid (Urho3D::Context *context);
+    virtual ~Urho3DHexagonalGrid () = default;
+
+    virtual void Setup ();
+    virtual void Start ();
+    virtual void Stop ();
+
+private:
+    void SetupLight ();
+    void SetupCamera ();
+    void LoadGrid ();
+    void SetupGrid ();
+
+    Urho3D::Scene *scene_;
+    DebugCamera *debugCamera_;
+    DoubledCoordsHexGrid *grid_;
+};
