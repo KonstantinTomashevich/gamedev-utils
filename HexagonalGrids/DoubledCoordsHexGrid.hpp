6e1dc8f22d11aa410c011d4961df645a80de076e
Konstantin Tomashevich <ekit.mail@gmail.com>
2018-07-12 11:14:09 +0300

    Add HexagonalGrids with DoubledCoordsHexGrid.

---
+++ HexagonalGrids/DoubledCoordsHexGrid.hpp
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <GraphPathfinding/GraphAdapter.hpp>
+#include <SharedPointer/SharedPointer.hpp>
+#include <SharedPointer/ReferenceCounted.hpp>
+
+#include <unordered_map>
+#include <vector>
+#include <functional>
+
+class DoubledCoordsHexGrid : public GraphAdapter
+{
+public:
+    typedef std::function <float (float from, float to)> CostCalculator;
+    static float MedianCostCalculator (float from, float to);
+
+    class DoubledCoordsHexGridNeighborsIterator : public SimpleIterator <VertexOutcomingConnection>
+    {
+    public:
+        DoubledCoordsHexGridNeighborsIterator (const DoubledCoordsHexGrid *ownerGrid,
+                unsigned int currentRow, unsigned int currentCol);
+        virtual ~DoubledCoordsHexGridNeighborsIterator () = default;
+
+        virtual void Increment ();
+        virtual VertexOutcomingConnection Get ();
+        virtual bool Valid ();
+
+        // Exceptions classes.
+        class Invalid;
+
+    private:
+        unsigned int moveIndex_;
+        unsigned int currentRow_;
+        unsigned int currentCol_;
+        const DoubledCoordsHexGrid *ownerGrid_;
+    };
+
+    enum class Type
+    {
+        DoubleWidth = 0,
+        DoubleHeight
+    };
+
+    DoubledCoordsHexGrid (Type type, unsigned int maxRow, unsigned int maxCol, float hexRadius,
+            const CostCalculator &costCalculator = MedianCostCalculator);
+
+    virtual SimpleIterator <VertexOutcomingConnection> *GetOutcomingConnections (int vertex) const;
+    virtual float HeuristicDistance (int beginVertex, int endVertex) const;
+
+    unsigned int EncodeCellPosition (unsigned int row, unsigned int col) const;
+    void DecodeCellPosition (unsigned int vertex, unsigned int &row, unsigned int &col) const;
+    const std::vector <std::pair <int, int> > *GetAvailableMovesList () const;
+    float GetCostBetween (unsigned int fromHash, unsigned int toHash) const;
+
+    Type GetType () const;
+    unsigned int GetMaxRow () const;
+    unsigned int GetMaxCol () const;
+    float GetHexRadius () const;
+
+    std::pair <float, float> GetCellPosition (unsigned int row, unsigned int col) const;
+    std::pair <float, float> GetCellPosition (unsigned int cell) const;
+
+    void SetCell (unsigned int row, unsigned int col, float costModifier, SharedPointer <ReferenceCounted> metadata);
+    void SetCell (unsigned int cell, float costModifier, SharedPointer <ReferenceCounted> metadata);
+
+    float GetCellCostModifier (unsigned int row, unsigned int col) const;
+    float GetCellCostModifier (unsigned int cell) const;
+
+    SharedPointer <ReferenceCounted> GetCellMetadata (unsigned int row, unsigned int col) const;
+    SharedPointer <ReferenceCounted> GetCellMetadata (unsigned int cell) const;
+
+    const std::unordered_map <unsigned int, float> &GetCostModifiers () const;
+    const std::unordered_map <unsigned int, SharedPointer <ReferenceCounted>> &GetMetadata () const;
+
+    // Exceptions classes.
+    class UnknownHash;
+
+private:
+    static const std::vector <std::pair <int, int> > DoubleWidthAvailableMoves;
+    static const std::vector <std::pair <int, int> > DoubleHeightAvailableMoves;
+
+    Type type_;
+    unsigned int maxRow_;
+    unsigned int maxCol_;
+    float hexRadius_;
+
+    CostCalculator costCalculator_;
+    std::unordered_map <unsigned int, float> costModifiers_;
+    std::unordered_map <unsigned int, SharedPointer <ReferenceCounted> > metadata_;
+};
